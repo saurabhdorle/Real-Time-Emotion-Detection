f40afe5410e46f6f27462d9a81b56e062a85ae61
Saurabh Dorle <sdd.saurabhd@gmail.com>
2019-01-15 15:45:38 +0530

    custom image detection & confusion matrix

--- emo_detect_model.py
+++ emo_detect_model.py
@@ -63,7 +63,7 @@ pixels_value = np.array(split_list, 'float32')
 # Normalize the values by dividing it with 255(max pixwl value)
 pixels_value /= 255 #normalizing inputs between [0, 1]
 """ or"""
-# data preprocessing per-channel normalization (subtract mean, divide by standard deviation)
+# Per-channel normalization (subtract mean, divide by standard deviation)
 pixels_value -= np.mean(pixels_value, axis=0)
 pixels_value /= np.std(pixels_value, axis=0)
 
@@ -112,6 +112,25 @@ model.add(Dropout(0.2))
 
 model.add(Dense(num_classes, activation = 'softmax'))
 
+'''
+gen = ImageDataGenerator()
+train_generator = gen.flow(X_train, y_train, batch_size=256)
+ 
+model.compile(loss='categorical_crossentropy' , optimizer=keras.optimizers.Adam(), 
+              metrics=['accuracy'])
+ 
+model.fit_generator(train_generator, steps_per_epoch=batch_size, epochs=5)
+
+
+train_score = model.evaluate(X_train, y_train, verbose=0)
+print('Train loss:', train_score[0])
+print('Train accuracy:', 100*train_score[1])
+ 
+test_score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', test_score[0])
+print('Test accuracy:', 100*test_score[1])
+'''
+
 model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
 
 model.fit(X_train, y_train, batch_size = batch_size, epochs = epochs)
@@ -127,3 +146,48 @@ model.save('emo_detect.h5')# acc = 69, 56.78
 # Loading save Model
 #model = load_model('emo_detect.h5')
 
+# Making the Confusion Matrix
+y_pred = model.predict(X_test)
+#y_pred = (y_pred > 0.5) 
+y_pred = y_pred.round()
+
+# Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
+
+
+# =============================================================================
+# Testing on Custom Image
+# =============================================================================
+
+
+#function for drawing bar chart for emotion preditions
+def emotion_analysis(emotions):
+    objects = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+    y_pos = np.arange(len(objects))
+    
+    plt.bar(y_pos, emotions, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('percentage')
+    plt.title('emotion')
+    
+    plt.show()
+
+
+img = image.load_img("smile.jpg", grayscale=True, target_size=(48, 48))
+ 
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis = 0)
+ 
+x /= 255
+ 
+custom = model.predict(x)
+
+emotion_analysis(custom[0])
+ 
+x = np.array(x, 'float32')
+x = x.reshape([48, 48]);
+ 
+plt.gray()
+plt.imshow(x)
+plt.show()    
